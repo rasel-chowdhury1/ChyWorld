ae4d5494b03838e3a485d9550d16b5a21396f04b
Rasel Chowdhury <rasel.chowdhury0001@gmail.com>
2024-11-17 20:36:19 +0600

    create database model for this project using prisma orm and postgresql with other works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ const Homepage = () => {
   return (
     <div className='flex gap-6 pt-6'>
       <div className="hidden xl:block w-[20%]">
-        <div>Left menu</div>
+      <LeftMenu type="home" />
       </div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div>stories</div>
